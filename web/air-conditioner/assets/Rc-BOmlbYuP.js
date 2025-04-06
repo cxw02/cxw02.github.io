262f11b51ee0b13870b31a290c2815ad2d971d5f
cxw02 <yg02@qq.com>
2025-04-06 11:43:14 +0800

    first commit

---
+++ web/air-conditioner/assets/Rc-BOmlbYuP.js
@@ -0,0 +1,1 @@
+import{u as t,j as e,R as o}from"./index-D_nB-Ce3.js";const r=()=>(t(),e.jsx(o,{}));export{r as default};
---
+++ web/air-conditioner/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/web/air-conditioner/sw.js', { scope: '/web/air-conditioner/' })})}
